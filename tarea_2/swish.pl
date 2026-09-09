e8e5664f729fd20a64257f18a52dcaaa6e2612f6
Enrique Ramirez <enrique.ramirez@ucr.ac.cr>
2026-09-09 17:45:34 -0600

    puede_combinar()

--- tarea_2/swish.pl
+++ tarea_2/swish.pl
@@ -1,307 +1,309 @@
-condition(hipertension)
-condition(arritmias)
-condition(insuficiencia_cardiaca)
-condition(antecedentes_infarto)
-condition(acv)
-condition(enfermedad_renal_cronica)
-condition(insuficiencia_renal)
-condition(calculos_renales)
-condition(higado_graso)
-condition(cirrosis)
-condition(hepatitis_cronica)
-condition(diabetes)
-condition(hipotiroidismo)
-condition(hipertiroidismo)
-condition(dislipidemia)
-condition(asma)
-condition(epoc)
-condition(apnea_del_sueno)
-condition(ulceras_gastricas)
-condition(ulceras)
-condition(erge)
-condition(enfermedad_crohn)
-condition(colitis_ulcerosa)
-condition(epilepsia)
-condition(depresion)
-condition(trastorno_bipolar)
-condition(parkinson)
-condition(alzheimer)
-condition(demencia)
-condition(lupus)
-condition(artritis_reumatoide)
-condition(vih)
-condition(cancer)
-condition(glaucoma)
-condition(hiperplasia_prostatica_benigna)
-condition(trastornos_coagulacion)
-condition(hemofilia)
-condition(embarazo)
-condition(lactancia_materna)
+condition(1).
+condition(2).
+condition(3).
+condition(4).
+condition(5).
+condition(6).
+condition(7).
+condition(8).
+condition(9).
+condition(10).
+condition(11).
+condition(12).
+condition(13).
+condition(14).
+condition(15).
+condition(16).
+condition(17).
+condition(18).
+condition(19).
+condition(20).
+condition(21).
+condition(22).
+condition(23).
+condition(24).
+condition(25).
+condition(26).
+condition(27).
+condition(28).
+condition(29).
+condition(30).
+condition(31).
+condition(32).
+condition(33).
+condition(34).
+condition(35).
+condition(36).
 
-medicamento(paracetamol)
-medicamento(ibuprofeno)
-medicamento(omeprazol)
-medicamento(amoxicilina)
-medicamento(losartan)
-medicamento(metformina)
-medicamento(salbutamol)
-medicamento(loratadina)
-medicamento(atorvastatina)
-medicamento(levotiroxina)
-medicamento(enalapril)
-medicamento(clonazepam)
-medicamento(sertralina)
-medicamento(ciprofloxacino)
-medicamento(naproxeno)
-medicamento(aspirina)
-medicamento(ketorolaco)
-medicamento(amlodipino)
-medicamento(metoprolol)
-medicamento(metoclopramida)
-medicamento(hidroclorotiazida)
-medicamento(tramadol)
-medicamento(prednisona)
-medicamento(alprazolam)
-medicamento(furosemida)
-medicamento(glibenclamida)
-medicamento(azitromicina)
-medicamento(clorfenamina)
-medicamento(butilhioscina)
-medicamento(tamsulosina)
-medicamento(montelukast)
-medicamento(simvastatina)
-medicamento(fluconazol)
-medicamento(sildenafilo)
-medicamento(dexketoprofeno)
+medicamento(paracetamol).
+medicamento(ibuprofeno).
+medicamento(omeprazol).
+medicamento(amoxicilina).
+medicamento(losartan).
+medicamento(metformina).
+medicamento(salbutamol).
+medicamento(loratadina).
+medicamento(atorvastatina).
+medicamento(levotiroxina).
+medicamento(enalapril).
+medicamento(clonazepam).
+medicamento(sertralina).
+medicamento(ciprofloxacino).
+medicamento(naproxeno).
+medicamento(aspirina).
+medicamento(ketorolaco).
+medicamento(amlodipino).
+medicamento(metoprolol).
+medicamento(metoclopramida).
+medicamento(hidroclorotiazida).
+medicamento(tramadol).
+medicamento(prednisona).
+medicamento(alprazolam).
+medicamento(furosemida).
+medicamento(glibenclamida).
+medicamento(azitromicina).
+medicamento(clorfenamina).
+medicamento(butilhioscina).
+medicamento(tamsulosina).
+medicamento(montelukast).
+medicamento(simvastatina).
+medicamento(fluconazol).
+medicamento(sildenafilo).
+medicamento(dexketoprofeno).
 
 
-ataca(paracetamol, dolor_leve_moderado)
-ataca(paracetamol, fiebre)
-ataca(ibuprofeno, dolor)
-ataca(ibuprofeno, inflamacion)
-ataca(ibuprofeno, fiebre)
-ataca(omeprazol, acidez)
-ataca(omeprazol, reflujo_gastrico)
-ataca(omeprazol, gastritis)
-ataca(amoxicilina, infecciones_bacterianas)
-ataca(losartan, hipertension_arterial)
-ataca(metformina, nivel_alto_azucar)
-ataca(salbutamol, broncoespasmo)
-ataca(salbutamol, sibilancias)
-ataca(salbutamol, falta_aire)
-ataca(loratadina, alergias)
-ataca(loratadina, rinitis)
-ataca(loratadina, picazon)
-ataca(loratadina, urticaria)
-ataca(atorvastatina, colesterol_elevado)
-ataca(levotiroxina, hipotiroidismo)
-ataca(levotiroxina, fatiga)
-ataca(levotiroxina, peso)
-ataca(enalapril, hipertension)
-ataca(enalapril, insuficiencia_cardiaca)
-ataca(clonazepam, ansiedad_severa)
-ataca(clonazepam, panico)
-ataca(clonazepam, convulsiones)
-ataca(sertralina, depresion)
-ataca(sertralina, panico)
-ataca(sertralina, ansiedad)
-ataca(sertralina, toc)
-ataca(ciprofloxacino, infecciones_bacterianas_complejas)
-ataca(naproxeno, dolor_articular)
-ataca(naproxeno, dolor_muscular)
-ataca(naproxeno, inflamacion)
-ataca(aspirina, dolor_leve)
-ataca(aspirina, fiebre)
-ataca(aspirina, riesgo_trombos)
-ataca(ketorolaco, dolor_agudo_moderado)
-ataca(ketorolaco, dolor_severo)
-ataca(amlodipino, hipertension_arterial)
-ataca(amlodipino, angina_pecho)
-ataca(metoprolol, hipertension)
-ataca(metoprolol, arritmias)
-ataca(metoprolol, angina)
-ataca(metoclopramida, nauseas)
-ataca(metoclopramida, vomitos)
-ataca(metoclopramida, vaciado_gastrico_lento)
-ataca(hidroclorotiazida, hipertension)
-ataca(hidroclorotiazida, retencion_liquidos)
-ataca(tramadol, dolor_moderado)
-ataca(tramadol, dolor_severo)
-ataca(prednisona, inflamacion_severa)
-ataca(prednisona, reacciones_autoinmunes)
-ataca(alprazolam, crisis_ansiedad)
-ataca(alprazolam, panico)
-ataca(furosemida, edema)
-ataca(furosemida, insuficiencia_cardiaca)
-ataca(glibenclamida, azucar_alta)
-ataca(azitromicina, infecciones_respiratorias)
-ataca(azitromicina, infecciones_piel)
-ataca(clorfenamina, sintomas_alergicos)
-ataca(clorfenamina, estornudos)
-ataca(clorfenamina, congestion)
-ataca(butilhioscina, colicos_abdominales)
-ataca(butilhioscina, espasmos_digestivos)
-ataca(tamsulosina, dificultad_orinar)
-ataca(montelukast, prevencion_asma)
-ataca(montelukast, rinitis_alergica)
-ataca(simvastatina, colesterol_alto)
-ataca(simvastatina, trigliceridos)
-ataca(fluconazol, infecciones_hongos)
-ataca(sildenafilo, disfuncion_erectil)
-ataca(sildenafilo, hipertension_pulmonar)
-ataca(dexketoprofeno, dolor_agudo_dental)
-ataca(dexketoprofeno, dolor_agudo_muscular)
-ataca(dexketoprofeno, dolor_agudo_menstrual)
+ataca(paracetamol, dolor_leve_moderado).
+ataca(paracetamol, fiebre).
+ataca(ibuprofeno, dolor).
+ataca(ibuprofeno, inflamacion).
+ataca(ibuprofeno, fiebre).
+ataca(omeprazol, acidez).
+ataca(omeprazol, reflujo_gastrico).
+ataca(omeprazol, gastritis).
+ataca(amoxicilina, infecciones_bacterianas).
+ataca(losartan, hipertension_arterial).
+ataca(metformina, nivel_alto_azucar).
+ataca(salbutamol, broncoespasmo).
+ataca(salbutamol, sibilancias).
+ataca(salbutamol, falta_aire).
+ataca(loratadina, alergias).
+ataca(loratadina, rinitis).
+ataca(loratadina, picazon).
+ataca(loratadina, urticaria).
+ataca(atorvastatina, colesterol_elevado).
+ataca(levotiroxina, hipotiroidismo).
+ataca(levotiroxina, fatiga).
+ataca(levotiroxina, peso).
+ataca(enalapril, hipertension).
+ataca(enalapril, insuficiencia_cardiaca).
+ataca(clonazepam, ansiedad_severa).
+ataca(clonazepam, panico).
+ataca(clonazepam, convulsiones).
+ataca(sertralina, depresion).
+ataca(sertralina, panico).
+ataca(sertralina, ansiedad).
+ataca(sertralina, toc).
+ataca(ciprofloxacino, infecciones_bacterianas_complejas).
+ataca(naproxeno, dolor_articular).
+ataca(naproxeno, dolor_muscular).
+ataca(naproxeno, inflamacion).
+ataca(aspirina, dolor_leve).
+ataca(aspirina, fiebre).
+ataca(aspirina, riesgo_trombos).
+ataca(ketorolaco, dolor_agudo_moderado).
+ataca(ketorolaco, dolor_severo).
+ataca(amlodipino, hipertension_arterial).
+ataca(amlodipino, angina_pecho).
+ataca(metoprolol, hipertension).
+ataca(metoprolol, arritmias).
+ataca(metoprolol, angina).
+ataca(metoclopramida, nauseas).
+ataca(metoclopramida, vomitos).
+ataca(metoclopramida, vaciado_gastrico_lento).
+ataca(hidroclorotiazida, hipertension).
+ataca(hidroclorotiazida, retencion_liquidos).
+ataca(tramadol, dolor_moderado).
+ataca(tramadol, dolor_severo).
+ataca(prednisona, inflamacion_severa).
+ataca(prednisona, reacciones_autoinmunes).
+ataca(alprazolam, crisis_ansiedad).
+ataca(alprazolam, panico).
+ataca(furosemida, edema).
+ataca(furosemida, insuficiencia_cardiaca).
+ataca(glibenclamida, azucar_alta).
+ataca(azitromicina, infecciones_respiratorias).
+ataca(azitromicina, infecciones_piel).
+ataca(clorfenamina, sintomas_alergicos).
+ataca(clorfenamina, estornudos).
+ataca(clorfenamina, congestion).
+ataca(butilhioscina, colicos_abdominales).
+ataca(butilhioscina, espasmos_digestivos).
+ataca(tamsulosina, dificultad_orinar).
+ataca(montelukast, prevencion_asma).
+ataca(montelukast, rinitis_alergica).
+ataca(simvastatina, colesterol_alto).
+ataca(simvastatina, trigliceridos).
+ataca(fluconazol, infecciones_hongos).
+ataca(sildenafilo, disfuncion_erectil).
+ataca(sildenafilo, hipertension_pulmonar).
+ataca(dexketoprofeno, dolor_agudo_dental).
+ataca(dexketoprofeno, dolor_agudo_muscular).
+ataca(dexketoprofeno, dolor_agudo_menstrual).
 
-contraindicacion(paracetamol, 9)
-contraindicacion(paracetamol, 8)
-contraindicacion(paracetamol, 10)
-contraindicacion(paracetamol, 6)
-contraindicacion(ibuprofeno, 18)
-contraindicacion(ibuprofeno, 19)
-contraindicacion(ibuprofeno, 20)
-contraindicacion(ibuprofeno, 21)
-contraindicacion(ibuprofeno, 5)
-contraindicacion(ibuprofeno, 6)
-contraindicacion(ibuprofeno, 1)
-contraindicacion(ibuprofeno, 3)
-contraindicacion(ibuprofeno, 4)
-contraindicacion(ibuprofeno, 33)
-contraindicacion(ibuprofeno, 34)
-contraindicacion(omeprazol, 29)
-contraindicacion(omeprazol, 34)
-contraindicacion(omeprazol, 35)
-contraindicacion(amoxicilina, 6)
-contraindicacion(amoxicilina, 9)
-contraindicacion(amoxicilina, 10)
-contraindicacion(losartan, 34)
-contraindicacion(losartan, 35)
-contraindicacion(losartan, 6)
-contraindicacion(metformina, 6)
-contraindicacion(metformina, 8)
-contraindicacion(metformina, 9)
-contraindicacion(metformina, 3)
-contraindicacion(metformina, 16)
-contraindicacion(salbutamol, 2)
-contraindicacion(salbutamol, 13)
-contraindicacion(salbutamol, 3)
-contraindicacion(loratadina, 9)
-contraindicacion(loratadina, 10)
-contraindicacion(loratadina, 31)
-contraindicacion(atorvastatina, 9)
-contraindicacion(atorvastatina, 10)
-contraindicacion(atorvastatina, 8)
-contraindicacion(atorvastatina, 34)
-contraindicacion(atorvastatina, 35)
-contraindicacion(levotiroxina, 13)
-contraindicacion(levotiroxina, 4)
-contraindicacion(enalapril, 34)
-contraindicacion(enalapril, 35)
-contraindicacion(enalapril, 6)
-contraindicacion(clonazepam, 17)
-contraindicacion(clonazepam, 16)
-contraindicacion(clonazepam, 31)
-contraindicacion(clonazepam, 9)
-contraindicacion(clonazepam, 34)
-contraindicacion(clonazepam, 35)
-contraindicacion(sertralina, 24)
-contraindicacion(sertralina, 22)
-contraindicacion(sertralina, 33)
-contraindicacion(sertralina, 9)
-contraindicacion(ciprofloxacino, 2)
-contraindicacion(ciprofloxacino, 22)
-contraindicacion(ciprofloxacino, 27)
-contraindicacion(ciprofloxacino, 28)
-contraindicacion(ciprofloxacino, 34)
-contraindicacion(ciprofloxacino, 35)
-contraindicacion(naproxeno, 18)
-contraindicacion(naproxeno, 20)
-contraindicacion(naproxeno, 21)
-contraindicacion(naproxeno, 6)
-contraindicacion(naproxeno, 1)
-contraindicacion(naproxeno, 33)
-contraindicacion(naproxeno, 34)
-contraindicacion(aspirina, 18)
-contraindicacion(aspirina, 33)
-contraindicacion(aspirina, 15)
-contraindicacion(aspirina, 6)
-contraindicacion(aspirina, 9)
-contraindicacion(aspirina, 34)
-contraindicacion(ketorolaco, 18)
-contraindicacion(ketorolaco, 19)
-contraindicacion(ketorolaco, 21)
-contraindicacion(ketorolaco, 6)
-contraindicacion(ketorolaco, 33)
-contraindicacion(ketorolaco, 4)
-contraindicacion(ketorolaco, 34)
-contraindicacion(amlodipino, 3)
-contraindicacion(amlodipino, 9)
-contraindicacion(amlodipino, 34)
-contraindicacion(metoprolol, 15)
-contraindicacion(metoprolol, 16)
-contraindicacion(metoprolol, 2)
-contraindicacion(metoprolol, 3)
-contraindicacion(metoclopramida, 25)
-contraindicacion(metoclopramida, 22)
-contraindicacion(metoclopramida, 18)
-contraindicacion(hidroclorotiazida, 6)
-contraindicacion(hidroclorotiazida, 7)
-contraindicacion(hidroclorotiazida, 9)
-contraindicacion(hidroclorotiazida, 11)
-contraindicacion(tramadol, 22)
-contraindicacion(tramadol, 17)
-contraindicacion(tramadol, 16)
-contraindicacion(tramadol, 9)
-contraindicacion(tramadol, 23)
-contraindicacion(prednisona, 11)
-contraindicacion(prednisona, 1)
-contraindicacion(prednisona, 18)
-contraindicacion(prednisona, 31)
-contraindicacion(prednisona, 29)
-contraindicacion(prednisona, 30)
-contraindicacion(alprazolam, 17)
-contraindicacion(alprazolam, 16)
-contraindicacion(alprazolam, 31)
-contraindicacion(alprazolam, 9)
-contraindicacion(alprazolam, 34)
-contraindicacion(furosemida, 6)
-contraindicacion(furosemida, 9)
-contraindicacion(furosemida, 7)
-contraindicacion(glibenclamida, 11)
-contraindicacion(glibenclamida, 6)
-contraindicacion(glibenclamida, 9)
-contraindicacion(glibenclamida, 34)
-contraindicacion(azitromicina, 2)
-contraindicacion(azitromicina, 9)
-contraindicacion(azitromicina, 10)
-contraindicacion(clorfenamina, 31)
-contraindicacion(clorfenamina, 32)
-contraindicacion(clorfenamina, 15)
-contraindicacion(clorfenamina, 16)
-contraindicacion(butilhioscina, 31)
-contraindicacion(butilhioscina, 32)
-contraindicacion(butilhioscina, 2)
-contraindicacion(tamsulosina, 9)
-contraindicacion(tamsulosina, 31)
-contraindicacion(montelukast, 23)
-contraindicacion(montelukast, 9)
-contraindicacion(simvastatina, 9)
-contraindicacion(simvastatina, 10)
-contraindicacion(simvastatina, 8)
-contraindicacion(simvastatina, 34)
-contraindicacion(simvastatina, 35)
-contraindicacion(fluconazol, 2)
-contraindicacion(fluconazol, 9)
-contraindicacion(fluconazol, 34)
-contraindicacion(sildenafilo, 4)
-contraindicacion(sildenafilo, 3)
-contraindicacion(sildenafilo, 2)
-contraindicacion(sildenafilo, 1)
-contraindicacion(dexketoprofeno, 18)
-contraindicacion(dexketoprofeno, 20)
-contraindicacion(dexketoprofeno, 21)
-contraindicacion(dexketoprofeno, 6)
-contraindicacion(dexketoprofeno, 9)
-contraindicacion(dexketoprofeno, 33)
-contraindicacion(dexketoprofeno, 15)
-contraindicacion(dexketoprofeno, 34)
+contraindicacion(paracetamol, 9).
+contraindicacion(paracetamol, 8).
+contraindicacion(paracetamol, 10).
+contraindicacion(paracetamol, 6).
+contraindicacion(ibuprofeno, 18).
+contraindicacion(ibuprofeno, 19).
+contraindicacion(ibuprofeno, 20).
+contraindicacion(ibuprofeno, 21).
+contraindicacion(ibuprofeno, 5).
+contraindicacion(ibuprofeno, 6).
+contraindicacion(ibuprofeno, 1).
+contraindicacion(ibuprofeno, 3).
+contraindicacion(ibuprofeno, 4).
+contraindicacion(ibuprofeno, 33).
+contraindicacion(ibuprofeno, 34).
+contraindicacion(omeprazol, 29).
+contraindicacion(omeprazol, 34).
+contraindicacion(omeprazol, 35).
+contraindicacion(amoxicilina, 6).
+contraindicacion(amoxicilina, 9).
+contraindicacion(amoxicilina, 10).
+contraindicacion(losartan, 34).
+contraindicacion(losartan, 35).
+contraindicacion(losartan, 6).
+contraindicacion(metformina, 6).
+contraindicacion(metformina, 8).
+contraindicacion(metformina, 9).
+contraindicacion(metformina, 3).
+contraindicacion(metformina, 16).
+contraindicacion(salbutamol, 2).
+contraindicacion(salbutamol, 13).
+contraindicacion(salbutamol, 3).
+contraindicacion(loratadina, 9).
+contraindicacion(loratadina, 10).
+contraindicacion(loratadina, 31).
+contraindicacion(atorvastatina, 9).
+contraindicacion(atorvastatina, 10).
+contraindicacion(atorvastatina, 8).
+contraindicacion(atorvastatina, 34).
+contraindicacion(atorvastatina, 35).
+contraindicacion(levotiroxina, 13).
+contraindicacion(levotiroxina, 4).
+contraindicacion(enalapril, 34).
+contraindicacion(enalapril, 35).
+contraindicacion(enalapril, 6).
+contraindicacion(clonazepam, 17).
+contraindicacion(clonazepam, 16).
+contraindicacion(clonazepam, 31).
+contraindicacion(clonazepam, 9).
+contraindicacion(clonazepam, 34).
+contraindicacion(clonazepam, 35).
+contraindicacion(sertralina, 24).
+contraindicacion(sertralina, 22).
+contraindicacion(sertralina, 33).
+contraindicacion(sertralina, 9).
+contraindicacion(ciprofloxacino, 2).
+contraindicacion(ciprofloxacino, 22).
+contraindicacion(ciprofloxacino, 27).
+contraindicacion(ciprofloxacino, 28).
+contraindicacion(ciprofloxacino, 34).
+contraindicacion(ciprofloxacino, 35).
+contraindicacion(naproxeno, 18).
+contraindicacion(naproxeno, 20).
+contraindicacion(naproxeno, 21).
+contraindicacion(naproxeno, 6).
+contraindicacion(naproxeno, 1).
+contraindicacion(naproxeno, 33).
+contraindicacion(naproxeno, 34).
+contraindicacion(aspirina, 18).
+contraindicacion(aspirina, 33).
+contraindicacion(aspirina, 15).
+contraindicacion(aspirina, 6).
+contraindicacion(aspirina, 9).
+contraindicacion(aspirina, 34).
+contraindicacion(ketorolaco, 18).
+contraindicacion(ketorolaco, 19).
+contraindicacion(ketorolaco, 21).
+contraindicacion(ketorolaco, 6).
+contraindicacion(ketorolaco, 33).
+contraindicacion(ketorolaco, 4).
+contraindicacion(ketorolaco, 34).
+contraindicacion(amlodipino, 3).
+contraindicacion(amlodipino, 9).
+contraindicacion(amlodipino, 34).
+contraindicacion(metoprolol, 15).
+contraindicacion(metoprolol, 16).
+contraindicacion(metoprolol, 2).
+contraindicacion(metoprolol, 3).
+contraindicacion(metoclopramida, 25).
+contraindicacion(metoclopramida, 22).
+contraindicacion(metoclopramida, 18).
+contraindicacion(hidroclorotiazida, 6).
+contraindicacion(hidroclorotiazida, 7).
+contraindicacion(hidroclorotiazida, 9).
+contraindicacion(hidroclorotiazida, 11).
+contraindicacion(tramadol, 22).
+contraindicacion(tramadol, 17).
+contraindicacion(tramadol, 16).
+contraindicacion(tramadol, 9).
+contraindicacion(tramadol, 23).
+contraindicacion(prednisona, 11).
+contraindicacion(prednisona, 1).
+contraindicacion(prednisona, 18).
+contraindicacion(prednisona, 31).
+contraindicacion(prednisona, 29).
+contraindicacion(prednisona, 30).
+contraindicacion(alprazolam, 17).
+contraindicacion(alprazolam, 16).
+contraindicacion(alprazolam, 31).
+contraindicacion(alprazolam, 9).
+contraindicacion(alprazolam, 34).
+contraindicacion(furosemida, 6).
+contraindicacion(furosemida, 9).
+contraindicacion(furosemida, 7).
+contraindicacion(glibenclamida, 11).
+contraindicacion(glibenclamida, 6).
+contraindicacion(glibenclamida, 9).
+contraindicacion(glibenclamida, 34).
+contraindicacion(azitromicina, 2).
+contraindicacion(azitromicina, 9).
+contraindicacion(azitromicina, 10).
+contraindicacion(clorfenamina, 31).
+contraindicacion(clorfenamina, 32).
+contraindicacion(clorfenamina, 15).
+contraindicacion(clorfenamina, 16).
+contraindicacion(butilhioscina, 31).
+contraindicacion(butilhioscina, 32).
+contraindicacion(butilhioscina, 2).
+contraindicacion(tamsulosina, 9).
+contraindicacion(tamsulosina, 31).
+contraindicacion(montelukast, 23).
+contraindicacion(montelukast, 9).
+contraindicacion(simvastatina, 9).
+contraindicacion(simvastatina, 10).
+contraindicacion(simvastatina, 8).
+contraindicacion(simvastatina, 34).
+contraindicacion(simvastatina, 35).
+contraindicacion(fluconazol, 2).
+contraindicacion(fluconazol, 9).
+contraindicacion(fluconazol, 34).
+contraindicacion(sildenafilo, 4).
+contraindicacion(sildenafilo, 3).
+contraindicacion(sildenafilo, 2).
+contraindicacion(sildenafilo, 1).
+contraindicacion(dexketoprofeno, 18).
+contraindicacion(dexketoprofeno, 20).
+contraindicacion(dexketoprofeno, 21).
+contraindicacion(dexketoprofeno, 6).
+contraindicacion(dexketoprofeno, 9).
+contraindicacion(dexketoprofeno, 33).
+contraindicacion(dexketoprofeno, 15).
+contraindicacion(dexketoprofeno, 34).
+
+% \+ sirve para verificar que no existen contraindicaciones en comun
+puede_combinar(A, B) :-
+    A \= B,
+    \+ ( contraindicacion(A, X), contraindicacion(B, X) ).
